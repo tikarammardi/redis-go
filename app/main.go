be0123120ebd3b4f5756137da75e4d94bc043e4a
Tikaram Mardi <tikarammardi007@gmail.com>
2025-10-04 12:56:28 +0530

    Implement connection handling and PING/PONG response

--- app/main.go
+++ app/main.go
@@ -21,13 +21,36 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
-	_, err = conn.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		return
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	for {
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			return
+		}
+		request := string(buf[:n])
+		if request == "*1\r\n$4\r\nPING\r\n" {
+			_, err := conn.Write([]byte("+PONG\r\n"))
+			if err != nil {
+				return
+			}
+		} else {
+			_, err := conn.Write([]byte("-ERR unknown command\r\n"))
+			if err != nil {
+				return
+			}
+		}
 	}
 }
