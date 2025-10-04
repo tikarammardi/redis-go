b434c958c05a3988f0510357babe06a84cb61483
Tikaram Mardi <tikarammardi007@gmail.com>
2025-10-04 16:07:21 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -98,171 +98,19 @@ func handleCommand(command RespValue, conn net.Conn) {
 	fmt.Println("Parts Debug:", parts)
 	switch strings.ToUpper(cmd) {
 	case "PING":
-		if len(parts) == 1 {
-			_, err := conn.Write([]byte("+PONG\r\n"))
-			if err != nil {
-				return
-			}
-		} else if len(parts) == 2 {
-			msg, ok := parts[1].Value.(string)
-			if !ok {
-				_, err := conn.Write([]byte("-ERR unknown command\r\n"))
-				if err != nil {
-					return
-				}
-				return
-			}
-			response := fmt.Sprintf("+%s\r\n", msg)
-			_, err := conn.Write([]byte(response))
-			if err != nil {
-				return
-			}
-		} else {
-			_, err := conn.Write([]byte("-ERR unknown command\r\n"))
-			if err != nil {
-				return
-			}
+		if handlePing(parts, conn) {
+			return
 		}
 	case "ECHO":
-		if len(parts) != 2 {
-			_, err := conn.Write([]byte("-ERR unknown command\r\n"))
-			if err != nil {
-				return
-			}
-			return
-		}
-		msg, ok := parts[1].Value.(string)
-		if !ok {
-			_, err := conn.Write([]byte("-ERR unknown command\r\n"))
-			if err != nil {
-				return
-			}
-			return
-		}
-		response := fmt.Sprintf("$%d\r\n%s\r\n", len(msg), msg)
-		_, err := conn.Write([]byte(response))
-		if err != nil {
+		if handleEcho(parts, conn) {
 			return
 		}
 	case "SET":
-		for i := range parts {
-			fmt.Printf("Part %d: Type %d, Value %v\n", i, parts[i].Type, parts[i].Value)
-		}
-		//if len(parts) != 3 {
-		//	_, err := conn.Write([]byte("-ERR unknown command\r\n"))
-		//	if err != nil {
-		//		return
-		//	}
-		//	return
-		//}
-		expiryCommands := []string{"EX", "PX"}
-		expiryValue := 0
-		for i := 3; i < len(parts); i += 2 {
-			partValue, ok := parts[i].Value.(string)
-			if !ok {
-				_, err := conn.Write([]byte("-ERR unknown command\r\n"))
-				if err != nil {
-					return
-				}
-				return
-			}
-			partValue = strings.ToUpper(partValue)
-			if !contains(expiryCommands, partValue) {
-				_, err := conn.Write([]byte("-ERR unknown command\r\n"))
-				if err != nil {
-					return
-				}
-				return
-			}
-			// if ex time will be in seconds and  px time will be in milliseconds
-
-			if i+1 >= len(parts) {
-				_, err := conn.Write([]byte("-ERR unknown command\r\n"))
-				if err != nil {
-					return
-				}
-				return
-			}
-			timeValue, ok := parts[i+1].Value.(string)
-			if !ok {
-				_, err := conn.Write([]byte("-ERR unknown command\r\n"))
-				if err != nil {
-					return
-				}
-				return
-			}
-			var timeInt int
-			_, err := fmt.Sscanf(timeValue, "%d", &timeInt)
-			if err != nil || timeInt <= 0 {
-				_, err := conn.Write([]byte("-ERR invalid expire time in set\r\n"))
-				if err != nil {
-					return
-				}
-				return
-			}
-			if partValue == "EX" {
-				expiryValue = timeInt * 1000 // convert to milliseconds
-			} else if partValue == "PX" {
-				expiryValue = timeInt
-			}
-		}
-
-		if len(parts) < 3 {
-			_, err := conn.Write([]byte("-ERR unknown command\r\n"))
-			if err != nil {
-				return
-			}
-			return
-		}
-		if parts[1].Type != BulkString || parts[2].Type != BulkString {
-			_, err := conn.Write([]byte("-ERR unknown command\r\n"))
-			if err != nil {
-				return
-			}
-			return
-		}
-		expiry := expiryValue
-		key := parts[1].Value.(string)
-		value := parts[2].Value.(string)
-		setValue(key, value, expiry)
-		fmt.Printf("Set key: %s to value: %s\n", key, value)
-		_, err := conn.Write([]byte("+OK\r\n"))
-		if err != nil {
+		if handleSet(parts, conn) {
 			return
 		}
 	case "GET":
-		if len(parts) != 2 {
-			_, err := conn.Write([]byte("-ERR unknown command\r\n"))
-			if err != nil {
-				return
-			}
-			return
-		}
-		key := parts[1].Value.(string)
-		value, exists := getValue(key)
-		fmt.Println("Exists:", exists, "Value:", value)
-
-		if !exists {
-
-			_, err := conn.Write([]byte("$-1\r\n")) // Null bulk string - https://redis.io/docs/latest/develop/reference/protocol-spec/#null-bulk-strings
-			if err != nil {
-				return
-			}
-			return
-
-			//value = "bar"
-			//response := fmt.Sprintf("$%d\r\n%s\r\n", len(key), key)
-			//_, err := conn.Write([]byte(response))
-			//if err != nil {
-			//	return
-			//}
-			//return
-		}
-
-		// if exist https://redis.io/docs/latest/develop/reference/protocol-spec/#bulk-strings
-		response := fmt.Sprintf("$%d\r\n%s\r\n", len(value), value)
-		_, err := conn.Write([]byte(response))
-		if err != nil {
+		if handleGet(parts, conn) {
 			return
 		}
 
@@ -273,6 +121,186 @@ func handleCommand(command RespValue, conn net.Conn) {
 		}
 	}
 
+}
+
+func handleGet(parts []RespValue, conn net.Conn) bool {
+	if len(parts) != 2 {
+		_, err := conn.Write([]byte("-ERR unknown command\r\n"))
+		if err != nil {
+			return true
+		}
+		return true
+	}
+	key := parts[1].Value.(string)
+	value, exists := getValue(key)
+	fmt.Println("Exists:", exists, "Value:", value)
+
+	if !exists {
+
+		_, err := conn.Write([]byte("$-1\r\n")) // Null bulk string - https://redis.io/docs/latest/develop/reference/protocol-spec/#null-bulk-strings
+		if err != nil {
+			return true
+		}
+		return true
+
+		//value = "bar"
+		//response := fmt.Sprintf("$%d\r\n%s\r\n", len(key), key)
+		//_, err := conn.Write([]byte(response))
+		//if err != nil {
+		//	return
+		//}
+		//return
+	}
+
+	// if exist https://redis.io/docs/latest/develop/reference/protocol-spec/#bulk-strings
+	response := fmt.Sprintf("$%d\r\n%s\r\n", len(value), value)
+	_, err := conn.Write([]byte(response))
+	if err != nil {
+		return true
+	}
+	return false
+}
+
+func handleSet(parts []RespValue, conn net.Conn) bool {
+	for i := range parts {
+		fmt.Printf("Part %d: Type %d, Value %v\n", i, parts[i].Type, parts[i].Value)
+	}
+	//if len(parts) != 3 {
+	//	_, err := conn.Write([]byte("-ERR unknown command\r\n"))
+	//	if err != nil {
+	//		return
+	//	}
+	//	return
+	//}
+	expiryCommands := []string{"EX", "PX"}
+	expiryValue := 0
+	for i := 3; i < len(parts); i += 2 {
+		partValue, ok := parts[i].Value.(string)
+		if !ok {
+			_, err := conn.Write([]byte("-ERR unknown command\r\n"))
+			if err != nil {
+				return true
+			}
+			return true
+		}
+		partValue = strings.ToUpper(partValue)
+		if !contains(expiryCommands, partValue) {
+			_, err := conn.Write([]byte("-ERR unknown command\r\n"))
+			if err != nil {
+				return true
+			}
+			return true
+		}
+		// if ex time will be in seconds and  px time will be in milliseconds
+
+		if i+1 >= len(parts) {
+			_, err := conn.Write([]byte("-ERR unknown command\r\n"))
+			if err != nil {
+				return true
+			}
+			return true
+		}
+		timeValue, ok := parts[i+1].Value.(string)
+		if !ok {
+			_, err := conn.Write([]byte("-ERR unknown command\r\n"))
+			if err != nil {
+				return true
+			}
+			return true
+		}
+		var timeInt int
+		_, err := fmt.Sscanf(timeValue, "%d", &timeInt)
+		if err != nil || timeInt <= 0 {
+			_, err := conn.Write([]byte("-ERR invalid expire time in set\r\n"))
+			if err != nil {
+				return true
+			}
+			return true
+		}
+		if partValue == "EX" {
+			expiryValue = timeInt * 1000 // convert to milliseconds
+		} else if partValue == "PX" {
+			expiryValue = timeInt
+		}
+	}
+
+	if len(parts) < 3 {
+		_, err := conn.Write([]byte("-ERR unknown command\r\n"))
+		if err != nil {
+			return true
+		}
+		return true
+	}
+	if parts[1].Type != BulkString || parts[2].Type != BulkString {
+		_, err := conn.Write([]byte("-ERR unknown command\r\n"))
+		if err != nil {
+			return true
+		}
+		return true
+	}
+	expiry := expiryValue
+	key := parts[1].Value.(string)
+	value := parts[2].Value.(string)
+	setValue(key, value, expiry)
+	fmt.Printf("Set key: %s to value: %s\n", key, value)
+	_, err := conn.Write([]byte("+OK\r\n"))
+	if err != nil {
+		return true
+	}
+	return false
+}
+
+func handleEcho(parts []RespValue, conn net.Conn) bool {
+	if len(parts) != 2 {
+		_, err := conn.Write([]byte("-ERR unknown command\r\n"))
+		if err != nil {
+			return true
+		}
+		return true
+	}
+	msg, ok := parts[1].Value.(string)
+	if !ok {
+		_, err := conn.Write([]byte("-ERR unknown command\r\n"))
+		if err != nil {
+			return true
+		}
+		return true
+	}
+	response := fmt.Sprintf("$%d\r\n%s\r\n", len(msg), msg)
+	_, err := conn.Write([]byte(response))
+	if err != nil {
+		return true
+	}
+	return false
+}
+
+func handlePing(parts []RespValue, conn net.Conn) bool {
+	if len(parts) == 1 {
+		_, err := conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			return true
+		}
+	} else if len(parts) == 2 {
+		msg, ok := parts[1].Value.(string)
+		if !ok {
+			_, err := conn.Write([]byte("-ERR unknown command\r\n"))
+			if err != nil {
+				return true
+			}
+			return true
+		}
+		response := fmt.Sprintf("+%s\r\n", msg)
+		_, err := conn.Write([]byte(response))
+		if err != nil {
+			return true
+		}
+	} else {
+		_, err := conn.Write([]byte("-ERR unknown command\r\n"))
+		if err != nil {
+			return true
+		}
+	}
+	return false
 }
 
 func contains(commands []string, value string) bool {
