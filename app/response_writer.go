b5179a03ddb6a282cac4d2b777a52aa070fa1e8e
Tikaram Mardi <tikarammardi007@gmail.com>
2025-10-08 22:41:09 +0530

    Enhance transaction response handling with ResponseCapture and WriteTransactionResults

--- app/response_writer.go
+++ app/response_writer.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 // RespResponseWriter implements ResponseWriter interface
@@ -15,166 +16,172 @@ func NewRespResponseWriter(conn net.Conn) *RespResponseWriter {
 	return &RespResponseWriter{conn: conn}
 }
 
-func (w *RespResponseWriter) WriteSimpleString(s string) error {
-	response := fmt.Sprintf("+%s\r\n", s)
+// writeResponse is a helper method to write the final response
+func (w *RespResponseWriter) writeResponse(response string) error {
 	_, err := w.conn.Write([]byte(response))
 	return err
 }
 
+// formatBulkString formats a bulk string with proper RESP format
+func formatBulkString(s string) string {
+	return fmt.Sprintf("$%d\r\n%s\r\n", len(s), s)
+}
+
+// formatNullBulkString formats a null bulk string
+func formatNullBulkString() string {
+	return "$-1\r\n"
+}
+
+// formatArrayHeader formats an array header
+func formatArrayHeader(length int) string {
+	return fmt.Sprintf("*%d\r\n", length)
+}
+
+func (w *RespResponseWriter) WriteSimpleString(s string) error {
+	response := fmt.Sprintf("+%s\r\n", s)
+	return w.writeResponse(response)
+}
+
 func (w *RespResponseWriter) WriteBulkString(s string) error {
-	response := fmt.Sprintf("$%d\r\n%s\r\n", len(s), s)
-	_, err := w.conn.Write([]byte(response))
-	return err
+	return w.writeResponse(formatBulkString(s))
 }
 
 func (w *RespResponseWriter) WriteInteger(i int) error {
 	response := fmt.Sprintf(":%d\r\n", i)
-	_, err := w.conn.Write([]byte(response))
-	return err
+	return w.writeResponse(response)
 }
 
 func (w *RespResponseWriter) WriteError(err string) error {
 	response := fmt.Sprintf("-%s\r\n", err)
-	_, err2 := w.conn.Write([]byte(response))
-	return err2
+	return w.writeResponse(response)
 }
 
 func (w *RespResponseWriter) WriteArray(items []string) error {
-	response := fmt.Sprintf("*%d\r\n", len(items))
+	var response strings.Builder
+	response.WriteString(formatArrayHeader(len(items)))
+
 	for _, item := range items {
-		response += fmt.Sprintf("$%d\r\n%s\r\n", len(item), item)
+		response.WriteString(formatBulkString(item))
 	}
-	_, err := w.conn.Write([]byte(response))
-	return err
+
+	return w.writeResponse(response.String())
 }
 
 func (w *RespResponseWriter) WriteNullBulkString() error {
-	_, err := w.conn.Write([]byte("$-1\r\n"))
-	return err
+	return w.writeResponse(formatNullBulkString())
 }
 
 func (w *RespResponseWriter) WriteNullArray() error {
-	_, err := w.conn.Write([]byte("*-1\r\n"))
-	return err
+	return w.writeResponse("*-1\r\n")
 }
 
 func (w *RespResponseWriter) WriteEmptyArray() error {
-	_, err := w.conn.Write([]byte("*0\r\n"))
-	return err
+	return w.writeResponse("*0\r\n")
+}
+
+// formatStreamEntry formats a single stream entry
+func formatStreamEntry(entry StreamEntry) string {
+	var response strings.Builder
+
+	// Each entry is an array of 2 elements: [id, [field1, value1, field2, value2, ...]]
+	response.WriteString("*2\r\n")
+	response.WriteString(formatBulkString(entry.ID))
+
+	// Format field-value pairs
+	fieldCount := len(entry.Fields) * 2
+	response.WriteString(formatArrayHeader(fieldCount))
+
+	for field, value := range entry.Fields {
+		response.WriteString(formatBulkString(field))
+		response.WriteString(formatBulkString(value))
+	}
+
+	return response.String()
 }
 
 func (w *RespResponseWriter) WriteStreamEntries(entries []StreamEntry) error {
-	// Write array length
-	response := fmt.Sprintf("*%d\r\n", len(entries))
+	var response strings.Builder
+	response.WriteString(formatArrayHeader(len(entries)))
 
 	for _, entry := range entries {
-		// Each entry is an array of 2 elements: [id, [field1, value1, field2, value2, ...]]
-		response += "*2\r\n" // Array of 2 elements
-
-		// First element: the ID as bulk string
-		response += fmt.Sprintf("$%d\r\n%s\r\n", len(entry.ID), entry.ID)
-
-		// Second element: array of field-value pairs
-		fieldCount := len(entry.Fields) * 2 // Each field has name and value
-		response += fmt.Sprintf("*%d\r\n", fieldCount)
-
-		// Add field-value pairs in order
-		for field, value := range entry.Fields {
-			response += fmt.Sprintf("$%d\r\n%s\r\n", len(field), field)
-			response += fmt.Sprintf("$%d\r\n%s\r\n", len(value), value)
-		}
+		response.WriteString(formatStreamEntry(entry))
 	}
 
-	_, err := w.conn.Write([]byte(response))
-	return err
+	return w.writeResponse(response.String())
+}
+
+// formatStreamReadResult formats a single stream read result
+func formatStreamReadResult(result StreamReadResult) string {
+	var response strings.Builder
+
+	// Each result is an array of 2 elements: [stream_key, [entries...]]
+	response.WriteString("*2\r\n")
+	response.WriteString(formatBulkString(result.Key))
+	response.WriteString(formatArrayHeader(len(result.Entries)))
+
+	for _, entry := range result.Entries {
+		response.WriteString(formatStreamEntry(entry))
+	}
+
+	return response.String()
 }
 
 func (w *RespResponseWriter) WriteStreamReadResults(results []StreamReadResult) error {
-	// Write array length for number of streams with results
-	response := fmt.Sprintf("*%d\r\n", len(results))
+	var response strings.Builder
+	response.WriteString(formatArrayHeader(len(results)))
 
 	for _, result := range results {
-		// Each result is an array of 2 elements: [stream_key, [entries...]]
-		response += "*2\r\n" // Array of 2 elements
-
-		// First element: the stream key as bulk string
-		response += fmt.Sprintf("$%d\r\n%s\r\n", len(result.Key), result.Key)
-
-		// Second element: array of entries
-		response += fmt.Sprintf("*%d\r\n", len(result.Entries))
-
-		for _, entry := range result.Entries {
-			// Each entry is an array of 2 elements: [id, [field1, value1, field2, value2, ...]]
-			response += "*2\r\n" // Array of 2 elements
-
-			// First element: the ID as bulk string
-			response += fmt.Sprintf("$%d\r\n%s\r\n", len(entry.ID), entry.ID)
-
-			// Second element: array of field-value pairs
-			fieldCount := len(entry.Fields) * 2 // Each field has name and value
-			response += fmt.Sprintf("*%d\r\n", fieldCount)
-
-			// Add field-value pairs in order
-			for field, value := range entry.Fields {
-				response += fmt.Sprintf("$%d\r\n%s\r\n", len(field), field)
-				response += fmt.Sprintf("$%d\r\n%s\r\n", len(value), value)
-			}
-		}
+		response.WriteString(formatStreamReadResult(result))
 	}
 
-	_, err := w.conn.Write([]byte(response))
-	return err
+	return w.writeResponse(response.String())
+}
+
+// formatRespValue formats a single RespValue for transaction results
+func formatRespValue(value RespValue) string {
+	switch value.Type {
+	case SimpleString:
+		return fmt.Sprintf("+%s\r\n", value.Value)
+	case BulkString:
+		if value.Value == nil {
+			return formatNullBulkString()
+		}
+		return formatBulkString(value.Value.(string))
+	case IntegerType:
+		return fmt.Sprintf(":%d\r\n", value.Value)
+	case ErrorType:
+		return fmt.Sprintf("-%s\r\n", value.Value)
+	case ArrayType:
+		return formatRespArray(value)
+	default:
+		return formatNullBulkString()
+	}
+}
+
+// formatRespArray formats an array RespValue
+func formatRespArray(value RespValue) string {
+	if value.Value == nil {
+		return "*-1\r\n"
+	}
+
+	arrayValues := value.Value.([]RespValue)
+	var response strings.Builder
+	response.WriteString(formatArrayHeader(len(arrayValues)))
+
+	for _, arrayItem := range arrayValues {
+		response.WriteString(formatRespValue(arrayItem))
+	}
+
+	return response.String()
 }
 
 func (w *RespResponseWriter) WriteTransactionResults(results []RespValue) error {
-	// Write array header
-	response := fmt.Sprintf("*%d\r\n", len(results))
+	var response strings.Builder
+	response.WriteString(formatArrayHeader(len(results)))
 
-	// Write each result in the array
 	for _, result := range results {
-		switch result.Type {
-		case SimpleString:
-			response += fmt.Sprintf("+%s\r\n", result.Value)
-		case BulkString:
-			if result.Value == nil {
-				response += "$-1\r\n"
-			} else {
-				s := result.Value.(string)
-				response += fmt.Sprintf("$%d\r\n%s\r\n", len(s), s)
-			}
-		case IntegerType:
-			response += fmt.Sprintf(":%d\r\n", result.Value)
-		case ErrorType:
-			response += fmt.Sprintf("-%s\r\n", result.Value)
-		case ArrayType:
-			if result.Value == nil {
-				response += "*-1\r\n"
-			} else {
-				arrayValues := result.Value.([]RespValue)
-				response += fmt.Sprintf("*%d\r\n", len(arrayValues))
-				// For nested arrays, we need to recursively serialize
-				for _, arrayItem := range arrayValues {
-					switch arrayItem.Type {
-					case BulkString:
-						if arrayItem.Value == nil {
-							response += "$-1\r\n"
-						} else {
-							s := arrayItem.Value.(string)
-							response += fmt.Sprintf("$%d\r\n%s\r\n", len(s), s)
-						}
-					case IntegerType:
-						response += fmt.Sprintf(":%d\r\n", arrayItem.Value)
-					case SimpleString:
-						response += fmt.Sprintf("+%s\r\n", arrayItem.Value)
-					case ErrorType:
-						response += fmt.Sprintf("-%s\r\n", arrayItem.Value)
-						// Add more cases as needed for complex nested structures
-					}
-				}
-			}
-		}
+		response.WriteString(formatRespValue(result))
 	}
 
-	_, err := w.conn.Write([]byte(response))
-	return err
+	return w.writeResponse(response.String())
 }
